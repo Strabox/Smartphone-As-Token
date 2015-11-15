04df187fa1cc1b46f094a3f5704934d3914c5c25
arturfonseca <artur.fonseca@live.com>
2015-11-15 01:12:06 +0000

    aaa
    aaa

--- app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartphoneastoken/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartphoneastoken/MainActivity.java
@@ -1,6 +1,12 @@
 package pt.ulisboa.tecnico.meic.sirs.smartphoneastoken;
 
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,15 +15,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+
+import java.util.List;
+import java.util.Set;
 
 import pt.ulisboa.tecnico.meic.sirs.smartphoneastoken.bluetooth.Bluetooth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Bluetooth bluetooth;
+    private final static int REQUEST_ENABLE_BT = 1;
+    private ArrayAdapter<String> mArrayAdapter;
 
-    public MainActivity(){
+    public MainActivity() {
         bluetooth = new Bluetooth(this);
     }
 
@@ -60,12 +72,53 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void buttonOnClick(View v){
+    public void buttonOnClick(View v) {
+
+        //http://developer.android.com/guide/topics/connectivity/bluetooth.html
         Button b = (Button) v;
         b.setText("Lol");
         bluetooth.turnOnBluetooth();
-    }
+       // mArrayAdapter= new ArrayAdapter<String>();
 
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+// If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+
+    }
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 
 
 }
