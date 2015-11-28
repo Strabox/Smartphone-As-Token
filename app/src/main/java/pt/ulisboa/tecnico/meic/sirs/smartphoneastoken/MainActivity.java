4450003fd723871859a34fd46db93fb861e3c600
Fabio Antunes <fabio.antunes@xpand-it.com>
2015-11-28 18:27:20 +0000

    Some SDK / JDK version fixed

--- app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartphoneastoken/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartphoneastoken/MainActivity.java
@@ -1,29 +1,15 @@
 package pt.ulisboa.tecnico.meic.sirs.smartphoneastoken;
 
-import android.app.AlertDialog;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.List;
-import java.util.Set;
+import android.view.View;
 
 import pt.ulisboa.tecnico.meic.sirs.smartphoneastoken.bluetooth.Bluetooth;
-import pt.ulisboa.tecnico.meic.sirs.smartphoneastoken.bluetooth.MakeClientConnection;
 
 public class MainActivity extends AppCompatActivity {
 
